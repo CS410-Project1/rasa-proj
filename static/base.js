674b0c5d4067e05b09e1e46e391282653dbeba58
ducks-dev <73856326+ducks-dev@users.noreply.github.com>
2021-12-21 17:17:41 -0500

    added more comments for base.js

--- static/base.js
+++ static/base.js
@@ -7,6 +7,7 @@ var data = {};
 var grab_new = true;
 const INTRO_MESSAGE = "Hello this is McDonalds from Washington Street, you worked for us before, would you like to come back?";
 
+// FUNCTIONS CALLED IN HTML FILE
 
 // PRE:  A set of phone numbers with area and nation codes delimited by whitespace or commas
 //       have been entered in the phone-text-area text box
@@ -42,7 +43,7 @@ const sendresponse = async() => {
 }
 
 // PRE:  
-// POST: 
+// POST: loops on 3 second timer for new user mesages from rasa
 const updatechat = async(msg) => {
     while(grab_new == false) {
         // holds code execution for 3 seconds
@@ -65,15 +66,15 @@ const updatechat = async(msg) => {
 }
 
 
-// PRE:  
-// POST: 
+// PRE:    
+// POST: loops till a new user is detected and then chat logs are displayed
 const updatequestion = async() =>  {
     if(grab_new) {
 	      data = await getData(data["SENDER_ID"]);
 	      data = JSON.parse(data);
 	      latestmsg = data["MESSAGE"]
 
-        // busy waits new user to need help "data to be updated"
+        // loops till data is updated with a new SENDER_ID
 	      while(data["SENDER_ID"] == "") {
 	          data = await getData()
 	          data = JSON.parse(data)
@@ -99,7 +100,8 @@ const updatequestion = async() =>  {
 }
 
 // PRE:  
-// POST: 
+// POST: displays bot response in text field and updates csv with the
+//       classification of the user intent
 const updateresponse = async() => {
     let response_dict = get_response_dict();
     let intent_dict = get_intent_dict();
@@ -133,7 +135,8 @@ const updateresponse = async() => {
 }
 
 // PRE:  
-// POST:
+// POST: deletes all session data on page and tells rasa it can resume conversation
+//       with the client
 const resumerasa = async () => {
     await fetch(RASA_URL + data["SENDER_ID"] + "/execute", {
         method: "POST",
@@ -150,10 +153,11 @@ const resumerasa = async () => {
 }
 
 
+
 // UTILITY FUNCTIONS
 
-// PRE:  
-// POST:
+// PRE:  msg must be a string
+// POST: displays a text msg in the bot side of the text screen
 function display_bot_text(msg) {
     let text = '<div class="chat-messages"><div class="chat-message-right pb-4 justify-content-start"><div></div><div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3"><div class="font-weight-bold mb-1">You</div>'
     text += msg;
@@ -163,8 +167,8 @@ function display_bot_text(msg) {
 
 }
 
-// PRE:  
-// POST:
+// PRE:  msg must be a string
+// POST: displays a text msg in the user side of the text screen
 function display_user_text(msg) {
     let text = '<div class="chat-messages"><div class="chat-message-left pb-4 justify-content-end"><div></div><div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3"><div class="font-weight-bold mb-1">'
     text += data["SENDER_ID"]
@@ -174,8 +178,8 @@ function display_user_text(msg) {
     document.getElementById("chat-history").innerHTML += text;
 }
 
-// PRE:  
-// POST:
+// PRE:  msg must be a string
+// POST: writes msg to the question field of the web page
 function update_user_question(msg) {    
 	  document.getElementById("question").innerHTML = '<strong>Question</strong>:- ' + msg;
 }
@@ -209,7 +213,7 @@ function clear_chat() {
 }
 
 // PRE:  
-// POST: 
+// POST: calls the /senddata api call with data_dict cast to a JSON object
 function update_csv(data_dict) {
     fetch(FLASK_URL + "/senddata", {
         method: "POST",
@@ -228,27 +232,28 @@ function update_csv(data_dict) {
 }
 
 // PRE:  
-// POST:
+// POST: the data global variable is reset to its default values
 function reset_data() {
     data = {'MESSAGE': "", 'SENDER_ID': "", 'FULL_CONV':"[]"};
 }
 
 // PRE:  
-// POST:
+// POST: 
 function grab_reset_field_by_id(id) {
     let val = document.getElementById(id).value
     document.getElementById(id).value = document.getElementById(id).defaultValue;
     return val;
 }
 
-// PRE:  
-// POST:
+// PRE:  numbers in phone text area field must be delimited by whitespace or commas
+//       the numbers must also be of the form 12223334444 or +12223334444
+// POST: returns a list of phone numbers in the phone text area field (or empty list)
 function get_phone_numbers() {
     return grab_reset_field_by_id("phone-text-area").split(/[ ,\\r\\n]+/);
 }
 
-// PRE:  
-// POST:
+// PRE:  data global variable must have been initialized
+// POST: displays all entries in the data[FULL_CONV] in the sms display
 function display_full_conv() {
     display_bot_text(INTRO_MESSAGE);
     for(let i = 0; i < data["FULL_CONV"].length; i++) {
@@ -286,7 +291,7 @@ function get_intent_dict() {
 		}
 }
 
-// PRE:  a phone number of the form +12223334444 or 12223334444
+// PRE:  number must be a valid phone number of the form +12223334444 or 12223334444
 //       (RASA service must have a tracker associated with the number)
 // POST: a tracker from Rasa that is associated with the number 
 function get_tracker(number) {
